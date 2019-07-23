2c8bf77022dab91cfa9ac82503e381347460144e
Twitch0125 <twitcherc@gmail.com>
2019-07-23 10:28:24 -0600

    added filter and details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { CharacterListComponent } from './characters/character-list/character-list.component';
-import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { CharacterListComponent } from "./characters/character-list/character-list.component";
+import { CommonModule } from "@angular/common";
+import {
+  MatListModule,
+  MatButtonModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatToolbarModule
+} from "@angular/material";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WelcomeComponent,
-    CharacterListComponent
-  ],
+  declarations: [AppComponent, WelcomeComponent, CharacterListComponent],
   imports: [
     BrowserModule,
     CommonModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatListModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/characters/character-list/character-list.component.ts
+++ src/app/characters/character-list/character-list.component.ts
@@ -1,304 +1,326 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
 @Component({
-  selector: 'swc-character-list',
-  templateUrl: './character-list.component.html',
-  styleUrls: ['./character-list.component.scss']
+  selector: "swc-character-list",
+  templateUrl: "./character-list.component.html",
+  styleUrls: ["./character-list.component.scss"]
 })
 export class CharacterListComponent implements OnInit {
-  title = 'Character List';
-
-  characters = [
+  title: string = "Character List";
+  showDetails: boolean = true;
+  filterBy: string = "";
+  charactersData = [
     {
-      id: 'luke-skywalker',
-      name: 'Luke Skywalker',
+      id: "luke-skywalker",
+      name: "Luke Skywalker",
       height: 172,
       mass: 77,
-      hair_color: 'blond',
-      skin_color: 'fair',
-      eye_color: 'blue',
-      birth_year: '19BBY',
-      gender: 'male',
+      hair_color: "blond",
+      skin_color: "fair",
+      eye_color: "blue",
+      birth_year: "19BBY",
+      gender: "male",
       force: 5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/luke-skywalker-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/luke-skywalker-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/luke-skywalker-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/luke-skywalker-lg.jpeg"
     },
     {
-      id: 'c-3po',
-      name: 'C-3PO',
+      id: "c-3po",
+      name: "C-3PO",
       height: 167,
       mass: 75,
-      hair_color: 'n/a',
-      skin_color: 'gold',
-      eye_color: 'yellow',
-      birth_year: '112BBY',
-      gender: 'n/a',
+      hair_color: "n/a",
+      skin_color: "gold",
+      eye_color: "yellow",
+      birth_year: "112BBY",
+      gender: "n/a",
       force: 0.5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/c-3po-lg.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/c-3po-lg.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/c-3po-sm.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/c-3po-sm.jpeg"
     },
     {
-      id: 'r2-d2',
-      name: 'R2-D2',
+      id: "r2-d2",
+      name: "R2-D2",
       height: 96,
       mass: 32,
-      hair_color: 'n/a',
-      skin_color: 'white, blue',
-      eye_color: 'red',
-      birth_year: '33BBY',
-      gender: 'n/a',
+      hair_color: "n/a",
+      skin_color: "white, blue",
+      eye_color: "red",
+      birth_year: "33BBY",
+      gender: "n/a",
       force: 0.5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/r2-d2-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/r2-d2-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/r2-d2-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/r2-d2-lg.jpeg"
     },
     {
-      id: 'darth-vader',
-      name: 'Darth Vader',
+      id: "darth-vader",
+      name: "Darth Vader",
       height: 202,
       mass: 136,
-      hair_color: 'none',
-      skin_color: 'white',
-      eye_color: 'yellow',
-      birth_year: '41.9BBY',
-      gender: 'male',
+      hair_color: "none",
+      skin_color: "white",
+      eye_color: "yellow",
+      birth_year: "41.9BBY",
+      gender: "male",
       force: 5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/darth-vader-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/darth-vader-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/darth-vader-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/darth-vader-lg.jpeg"
     },
     {
-      id: 'princess-leia',
-      name: 'Leia Organa',
+      id: "princess-leia",
+      name: "Leia Organa",
       height: 150,
       mass: 49,
-      hair_color: 'brown',
-      skin_color: 'light',
-      eye_color: 'brown',
-      birth_year: '19BBY',
-      gender: 'female',
+      hair_color: "brown",
+      skin_color: "light",
+      eye_color: "brown",
+      birth_year: "19BBY",
+      gender: "female",
       force: 5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/princess-leia-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/princess-leia-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/princess-leia-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/princess-leia-lg.jpeg"
     },
     {
-      id: 'anakin-skywalker',
-      name: 'Anakin Skywalker',
+      id: "anakin-skywalker",
+      name: "Anakin Skywalker",
       height: 188,
       mass: 84,
-      hair_color: 'blond',
-      skin_color: 'fair',
-      eye_color: 'blue',
-      birth_year: '41.9BBY',
-      gender: 'male',
+      hair_color: "blond",
+      skin_color: "fair",
+      eye_color: "blue",
+      birth_year: "41.9BBY",
+      gender: "male",
       force: 5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/anakin-skywalker-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/anakin-skywalker-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/anakin-skywalker-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/anakin-skywalker-lg.jpeg"
     },
     {
-      id: 'chewie',
-      name: 'Chewbacca',
+      id: "chewie",
+      name: "Chewbacca",
       height: 228,
       mass: 112,
-      hair_color: 'brown',
-      skin_color: 'unknown',
-      eye_color: 'blue',
-      birth_year: '200BBY',
-      gender: 'male',
+      hair_color: "brown",
+      skin_color: "unknown",
+      eye_color: "blue",
+      birth_year: "200BBY",
+      gender: "male",
       force: 0.5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/chewie-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/chewie-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/chewie-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/chewie-lg.jpeg"
     },
     {
-      id: 'han-solo',
-      name: 'Han Solo',
+      id: "han-solo",
+      name: "Han Solo",
       height: 180,
       mass: 80,
-      hair_color: 'brown',
-      skin_color: 'fair',
-      eye_color: 'brown',
-      birth_year: '29BBY',
-      gender: 'male',
+      hair_color: "brown",
+      skin_color: "fair",
+      eye_color: "brown",
+      birth_year: "29BBY",
+      gender: "male",
       force: 0.5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/han-solo-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/han-solo-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/han-solo-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/han-solo-lg.jpeg"
     },
     {
-      id: 'jabba-the-hutt',
-      name: 'Jabba Desilijic Tiure',
+      id: "jabba-the-hutt",
+      name: "Jabba Desilijic Tiure",
       height: 175,
       mass: 1358,
-      hair_color: 'n/a',
-      skin_color: 'green-tan, brown',
-      eye_color: 'orange',
-      birth_year: '600BBY',
-      gender: 'hermaphrodite',
+      hair_color: "n/a",
+      skin_color: "green-tan, brown",
+      eye_color: "orange",
+      birth_year: "600BBY",
+      gender: "hermaphrodite",
       force: 0.5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/jabba-the-hutt-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/jabba-the-hutt-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/jabba-the-hutt-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/jabba-the-hutt-lg.jpeg"
     },
     {
-      id: 'jyn-erso',
-      name: 'Jyn Erso',
+      id: "jyn-erso",
+      name: "Jyn Erso",
       height: 170,
       mass: 77,
-      hair_color: 'brown',
-      skin_color: 'fair',
-      eye_color: 'green',
-      birth_year: '22BBY',
-      gender: 'female',
+      hair_color: "brown",
+      skin_color: "fair",
+      eye_color: "green",
+      birth_year: "22BBY",
+      gender: "female",
       force: 0.5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/jyn-erso-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/jyn-erso-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/jyn-erso-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/jyn-erso-lg.jpeg"
     },
     {
-      id: 'kylo-ren',
-      name: 'Kylo Ren',
+      id: "kylo-ren",
+      name: "Kylo Ren",
       height: 180,
       mass: 110,
-      hair_color: 'black',
-      skin_color: 'fair',
-      eye_color: 'hazel',
-      birth_year: '5ABY',
-      gender: 'male',
+      hair_color: "black",
+      skin_color: "fair",
+      eye_color: "hazel",
+      birth_year: "5ABY",
+      gender: "male",
       force: 4,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/kylo-ren-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/kylo-ren-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/kylo-ren-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/kylo-ren-lg.jpeg"
     },
     {
-      id: 'yoda',
-      name: 'Yoda',
+      id: "yoda",
+      name: "Yoda",
       height: 66,
       mass: 17,
-      hair_color: 'white',
-      skin_color: 'green',
-      eye_color: 'brown',
-      birth_year: '896BBY',
-      gender: 'male',
+      hair_color: "white",
+      skin_color: "green",
+      eye_color: "brown",
+      birth_year: "896BBY",
+      gender: "male",
       force: 5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/yoda-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/yoda-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/yoda-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/yoda-lg.jpeg"
     },
     {
-      id: 'cassian-andor',
-      name: 'Cassian Andor',
+      id: "cassian-andor",
+      name: "Cassian Andor",
       height: 206,
       mass: 80,
-      hair_color: 'brown',
-      skin_color: 'light brown',
-      eye_color: 'brown',
-      birth_year: '26BBY',
-      gender: 'male',
+      hair_color: "brown",
+      skin_color: "light brown",
+      eye_color: "brown",
+      birth_year: "26BBY",
+      gender: "male",
       force: 0.5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/cassian-andor-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/cassian-andor-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/cassian-andor-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/cassian-andor-lg.jpeg"
     },
     {
-      id: 'finn',
-      name: 'Finn',
+      id: "finn",
+      name: "Finn",
       height: -1,
       mass: -1,
-      hair_color: 'black',
-      skin_color: 'dark',
-      eye_color: 'dark',
-      birth_year: 'unknown',
-      gender: 'male',
+      hair_color: "black",
+      skin_color: "dark",
+      eye_color: "dark",
+      birth_year: "unknown",
+      gender: "male",
       force: 2.5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/finn-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/finn-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/finn-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/finn-lg.jpeg"
     },
     {
-      id: 'rey',
-      name: 'Rey',
+      id: "rey",
+      name: "Rey",
       height: -1,
       mass: -1,
-      hair_color: 'brown',
-      skin_color: 'light',
-      eye_color: 'hazel',
-      birth_year: 'unknown',
-      gender: 'female',
+      hair_color: "brown",
+      skin_color: "light",
+      eye_color: "hazel",
+      birth_year: "unknown",
+      gender: "female",
       force: 5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/rey-sm.jpeg',
-      img: 'https://s3-us-west-2.amazonaws.com/star-wars-characters/rey-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/rey-sm.jpeg",
+      img: "https://s3-us-west-2.amazonaws.com/star-wars-characters/rey-lg.jpeg"
     },
     {
-      id: 'poe-dameron',
-      name: 'Poe Dameron',
+      id: "poe-dameron",
+      name: "Poe Dameron",
       height: -1,
       mass: -1,
-      hair_color: 'brown',
-      skin_color: 'light',
-      eye_color: 'brown',
-      birth_year: 'unknown',
-      gender: 'male',
+      hair_color: "brown",
+      skin_color: "light",
+      eye_color: "brown",
+      birth_year: "unknown",
+      gender: "male",
       force: 0.5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/poe-dameron-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/poe-dameron-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/poe-dameron-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/poe-dameron-lg.jpeg"
     },
     {
-      id: 'bb-8',
-      name: 'BB8',
+      id: "bb-8",
+      name: "BB8",
       height: -1,
       mass: -1,
-      hair_color: 'none',
-      skin_color: 'none',
-      eye_color: 'black',
-      birth_year: 'unknown',
-      gender: 'none',
+      hair_color: "none",
+      skin_color: "none",
+      eye_color: "black",
+      birth_year: "unknown",
+      gender: "none",
       force: 0.5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/bb-8-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/bb-8-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/bb-8-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/bb-8-lg.jpeg"
     },
     {
-      id: 'padme-amidala',
-      name: 'Padmé Amidala',
+      id: "padme-amidala",
+      name: "Padmé Amidala",
       height: 165,
       mass: 45,
-      hair_color: 'brown',
-      skin_color: 'light',
-      eye_color: 'brown',
-      birth_year: '46BBY',
-      gender: 'female',
+      hair_color: "brown",
+      skin_color: "light",
+      eye_color: "brown",
+      birth_year: "46BBY",
+      gender: "female",
       force: 0.5,
       avatar:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/padme-amidala-sm.jpeg',
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/padme-amidala-sm.jpeg",
       img:
-        'https://s3-us-west-2.amazonaws.com/star-wars-characters/padme-amidala-lg.jpeg'
+        "https://s3-us-west-2.amazonaws.com/star-wars-characters/padme-amidala-lg.jpeg"
     }
   ];
 
+  characters = this.charactersData;
+
   constructor() {}
+
+  public toggleDetails() {
+    if (this.showDetails === true) {
+      this.showDetails = false;
+    } else {
+      this.showDetails = true;
+    }
+
+    console.log(this.filterBy);
+  }
+
+  public filterCharacters() {
+    if (this.filterBy == "") {
+      this.characters = this.charactersData;
+    } else {
+      this.characters = this.charactersData.filter(character =>
+        character.name.toLowerCase().includes(this.filterBy.toLowerCase())
+      );
+    }
+  }
 
   ngOnInit() {}
 }
